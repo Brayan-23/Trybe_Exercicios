96c2bb98e7bb60e4552771f5d31e718b7e1d65de
Brayan Santos <brayan.santosrosa@gmail.com>
2022-04-27 20:36:46 -0300

    Realizando o Exercicio do dia 6.3 Bibliotecas.

---
+++ Fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-2-bibliotecas-javascript-e-frameworks-css/script.js
@@ -0,0 +1,25 @@
+const pegaBotao = document.getElementById('botao');
+pegaBotao.addEventListener('click', tirarEfeito);
+function tirarEfeito(){
+    event.preventDefault();
+}
+let pegaInputs = document.querySelectorAll('input');
+const pegaLimpeza = document.querySelector('#limpeza');
+let pegaAreaTxt = document.querySelector('#box-text') 
+pegaLimpeza.addEventListener('click',limpaCampos);
+
+function limpaCampos(){
+    tirarEfeito();
+    for(let passa = 0; passa < pegaInputs.length; passa += 1){
+        if(pegaInputs[passa].value !== '' || pegaInputs[passa].checked === true || pegaAreaTxt.value !== ''){
+            pegaInputs[passa].value = '';
+            pegaInputs[passa].checked = false;
+            pegaAreaTxt.value = '';
+        }
+    }
+}
+
+
+import JustValidate from 'just-validate';
+
+const validate = new JustValidate('#form');
