a2e4152fe0e810566e5ba6f7fdb9f84705a86b16
Brayan Santos <brayan.santosrosa@gmail.com>
2022-05-10 21:19:54 -0300

    Requesito 02 dos exercicios do dia conluido.

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const sum = require('./sum');
+const {sum, myRemove} = require('./sum');
 
 describe ('Testes referentes a função sum', () => {
     it('Return result 4 + 5 is 9', () =>{
@@ -13,4 +13,18 @@ describe ('Testes referentes a função sum', () => {
     it('Return result 4 + "5" is parameters must be numbers', () =>{
         expect(() => sum(4, "5")).toThrow('parameters must be numbers');
     })
+});
+//Parte 02
+describe('Testes referentes a função myRemove', () => {
+    it('Verefica se  [1, 2, 3, 4], 3 retorna ele mesmo', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+    });
+    
+    it('Verefica se [1, 2, 3, 4], 3 não retorna [1, 2, 3, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
+    });
+
+    it('Verefica se [1, 2, 3, 4], 5 não retorna [1, 2, 3, 4]', () => {
+        expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
+    });
 });
