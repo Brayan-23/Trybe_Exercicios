28afc1529d437409f9b15dbfa2f55f2cd2d44c31
Brayan Santos <brayan.santosrosa@gmail.com>
2022-05-10 21:36:38 -0300

    Requisito 03 dos exercicios do dia conluido.

--- Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,4 +1,4 @@
-const {sum, myRemove} = require('./sum');
+const {sum, myRemove, myFizzBuzz} = require('./sum');
 
 describe ('Testes referentes a função sum', () => {
     it('Return result 4 + 5 is 9', () =>{
@@ -19,7 +19,7 @@ describe('Testes referentes a função myRemove', () => {
     it('Verefica se  [1, 2, 3, 4], 3 retorna ele mesmo', () => {
         expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
     });
-    
+
     it('Verefica se [1, 2, 3, 4], 3 não retorna [1, 2, 3, 4]', () => {
         expect(myRemove([1, 2, 3, 4], 3)).not.toEqual([1, 2, 3, 4]);
     });
@@ -28,3 +28,17 @@ describe('Testes referentes a função myRemove', () => {
         expect(myRemove([1, 2, 3, 4], 5)).toEqual([1, 2, 3, 4]);
     });
 });
+describe('Testes referentes a função myFizzBuzz', () => {
+    it('Vereifica se o número divisivel 3 e 5', () => {
+        expect(myFizzBuzz(15)).toBe('fizzbuzz')
+    });
+    it('Vereifica se o número divisivel 3 apenas', () => {
+        expect(myFizzBuzz(9)).toBe('fizz')
+    });
+    it('Vereifica se o número não é divisivel nem 3 ou 5', () => {
+        expect(myFizzBuzz(16)).toBe(16);
+    });
+    it('Verifica se é um número', () => {
+        expect(myFizzBuzz('5')).toBeFalsy();
+    });
+})
